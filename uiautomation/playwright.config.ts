0d59f30dbb68590eb1c2de1a3ebd3826b1c32d0e
Andrii_Frolov <andrii.rolov@pearson.com>
2023-05-25 15:22:20 +0300

    added UI tests

--- uiautomation/playwright.config.ts
+++ uiautomation/playwright.config.ts
@@ -29,7 +29,7 @@ export default defineConfig({
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'retain-on-failure',
     screenshot: 'only-on-failure',
-    headless: false
+    headless: true
   },
 
   /* Configure projects for major browsers */
