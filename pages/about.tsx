af05434e52d2cdbe129be609a3e767bb14c4779b
Andrii_Frolov <andrii.rolov@pearson.com>
2023-07-06 15:00:00 +0300

    simple commit to trigger autotests

--- pages/about.tsx
+++ pages/about.tsx
@@ -1,7 +1,7 @@
 import { Typography } from "../components/Typography"
 
 export default function AboutPage() {
-    const text = 'About page 2'
+    const text = 'About page 3'
 
     return (
         <Typography text={text} />
